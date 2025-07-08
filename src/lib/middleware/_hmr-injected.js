504cefbc29d1774d296d1eab95ebf46ec0914b53
razmiO <thomas.cuesta31@gmail.com>
2025-07-08 12:48:49 +0200

    feat: add HotModule class and hmrClient function

--- src/lib/middleware/_hmr-injected.js
+++ src/lib/middleware/_hmr-injected.js
@@ -1,1 +1,30 @@
-console.log("Hello from the server");
+class HotModule {
+    file;
+    cb;
+    constructor(file) {
+        this.file = file;
+    }
+
+    accept(cb) {
+        console.log("accept " + this.file);
+        this.cb = cb;
+    }
+
+    handleAccept() {
+        if (!this.cb) return;
+        import(`${this.file}`).then((newModule) => {
+            this.cb(newModule);
+        });
+    }
+}
+
+function hmrClient(mod) {
+    const url = new URL(mod.url);
+    const hot = new HotModule(url.pathname);
+    console.log(hot);
+    import.meta.hot = hot;
+    setTimeout(() => {
+        hot.handleAccept();
+    }, 1000);
+    //
+}
